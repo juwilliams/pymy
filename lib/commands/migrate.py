b6993fbe519ba3e7d074487d948a5bba3f20f813
juwilliams <justin.williams@rpdmsolutions.com>
2016-05-17 15:42:12 -0400

    using csv lib now

--- lib/commands/migrate.py
+++ lib/commands/migrate.py
@@ -7,6 +7,7 @@ description:
 
 """
 
+import csv
 import pymysql.cursors
 import pymysql.converters
 
@@ -19,10 +20,6 @@ class Migrate(BaseCommand):
 		from lib.models import GlobalConfig
 		from lib.models import BaseModel
 		
-		rows = ()
-		row_delimiter = "\r\n"
-		col_delimiter = ","
-
 		# load the global config
 		config = GlobalConfig.load()
 
@@ -38,51 +35,51 @@ class Migrate(BaseCommand):
                              		cursorclass=pymysql.cursors.DictCursor)
 
 		try:
-			# retrieve table rows
-			with connection.cursor() as cursor:
-				cols = self.getCols(migration)
-				cols = (col_delimiter.join(cols),)
+			with open(migration.type + '.csv', 'wb') as csvfile:
+				csv_writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
 
-				# add header row
-				rows = rows + cols
-				
-				# get search cursor
-				cursor.execute(migration.query)
+				# retrieve table rows
+				with connection.cursor() as cursor:
+					csv_writer.writerow(self.getCols(migration))
+					
+					# get search cursor
+					cursor.execute(migration.query)
 
-				# iterate over rows and create insert statements
-				result = cursor.fetchone()			
-				while result is not None:
-					vals = ()
-
-					for mapping in migration.mappings:
-						try:
-							raw_val = result[mapping.field_from]
-						except KeyError:
-							raw_val = mapping.default if mapping.default is not None else '' 
-						finally:
-							if raw_val is None and mapping.default != '':
-								raw_val = mapping.default
-
-						encoded_val = pymysql.converters.escape_item(raw_val, 'utf-8').encode('ascii', 'xmlcharrefreplace')
-						encoded_val = encoded_val.replace('"', '""')
-						encoded_val = encoded_val.replace(',', '","')
-
-						vals = vals + (encoded_val,)
-						
+					# iterate over rows and create insert statements
 					result = cursor.fetchone()
 
-					# add value row
-					rows = rows + (col_delimiter.join(vals),)
+					while result is not None:
+						vals = []
+
+						for mapping in migration.mappings:
+							
+							try:
+								raw_val = result[mapping.field_from]
+							except KeyError:
+								raw_val = mapping.default if mapping.default is not None else '' 
+							finally:
+								if raw_val is None and mapping.default != '':
+									raw_val = mapping.default
+
+							encoded_val = pymysql.converters.escape_item(raw_val, 'utf-8').encode('ascii', 'xmlcharrefreplace')
+							encoded_val = encoded_val.replace("\"", "\"\"")
+							encoded_val = encoded_val[1:-1]
+
+							vals.append(encoded_val)
+							
+						result = cursor.fetchone()
+
+						# add value row
+						csv_writer.writerow(vals)
+		except Exception as e:
+			print e.message
 		finally:
-			connection.close()
-			
-			# write the output to csv
-			BaseModel.writeRaw(migration.type + '.csv', row_delimiter.join(rows))
+			connection.close()			
 
 	def getCols(self, migration):
-		cols = ()
+		cols = []
 
 		for mapping in migration.mappings:
-			cols = cols + (mapping.field_to,)
+			cols.append(mapping.field_to)
 
 		return cols
